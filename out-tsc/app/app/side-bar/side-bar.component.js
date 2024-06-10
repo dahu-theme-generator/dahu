c5853ae5411edb1f6de46de79d1e90d32877ce1a
mariusuktveris <520639@student.fontys.nl>
2024-06-10 17:24:47 +0200

    .

--- out-tsc/app/app/side-bar/side-bar.component.js
+++ out-tsc/app/app/side-bar/side-bar.component.js
@@ -1,16 +1,19 @@
 import { __decorate } from "tslib";
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { ContentViewComponent } from '../content-view/content-view.component';
+import { RouterModule } from '@angular/router';
 let SideBarComponent = class SideBarComponent {
 };
 SideBarComponent = __decorate([
     Component({
         selector: 'app-side-bar',
         standalone: true,
-        imports: [MatSidenavModule, ContentViewComponent],
+        imports: [MatSidenavModule, ContentViewComponent, RouterModule, MatListModule],
         templateUrl: './side-bar.component.html',
         styleUrl: './side-bar.component.scss'
+        //styleUrl: '../../styles.css'
     })
 ], SideBarComponent);
 export { SideBarComponent };
