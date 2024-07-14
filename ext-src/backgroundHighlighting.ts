e1b1f6917776a9bebd0cb89b57d3a3cb6560fe55
andojasl <a.lapinskas@student.fontys.nl>
2024-07-14 11:45:05 +0300

    remove unnecessary methods in backroundHighlighing

--- ext-src/backgroundHighlighting.ts
+++ ext-src/backgroundHighlighting.ts
@@ -79,62 +79,62 @@ function updateTokenColorCustomization(newTokenColors: { [key: string]: any }) {
 
 
 
-function changeEditor(color: string, context: vscode.ExtensionContext) {
-    const config = vscode.workspace.getConfiguration();
-    // const currentTheme = config.get<string>('workbench.colorTheme');
-    const themePath = path.join(context.extensionPath, 'themes', 'extensiontheme.json');
+// function changeEditor(color: string, context: vscode.ExtensionContext) {
+//     const config = vscode.workspace.getConfiguration();
+//     // const currentTheme = config.get<string>('workbench.colorTheme');
+//     const themePath = path.join(context.extensionPath, 'themes', 'extensiontheme.json');
 
-    try {
-        const themeData = JSON.parse(fs.readFileSync(themePath, 'utf-8'));
-        themeData.colors['editor.background'] = color;
-        fs.writeFileSync(themePath, JSON.stringify(themeData, null, 2), 'utf-8');
-    } catch (err) {
-        console.log('error while setting editor background: ' + (err as Error).message);
-    }
-}
+//     try {
+//         const themeData = JSON.parse(fs.readFileSync(themePath, 'utf-8'));
+//         themeData.colors['editor.background'] = color;
+//         fs.writeFileSync(themePath, JSON.stringify(themeData, null, 2), 'utf-8');
+//     } catch (err) {
+//         console.log('error while setting editor background: ' + (err as Error).message);
+//     }
+// }
 
-function changeSidebar(color: string, context: vscode.ExtensionContext) {
-    const themePath = path.join(context.extensionPath, 'themes', 'extensiontheme.json');
-    try {
-        const themeData = JSON.parse(fs.readFileSync(themePath, 'utf-8'));
-        themeData.colors['sideBar.background'] = color;
-        fs.writeFileSync(themePath, JSON.stringify(themeData, null, 2), 'utf-8');
-    } catch (err) {
-        console.log('error while setting side bar background: ' + (err as Error).message);
-    }
-}
+// function changeSidebar(color: string, context: vscode.ExtensionContext) {
+//     const themePath = path.join(context.extensionPath, 'themes', 'extensiontheme.json');
+//     try {
+//         const themeData = JSON.parse(fs.readFileSync(themePath, 'utf-8'));
+//         themeData.colors['sideBar.background'] = color;
+//         fs.writeFileSync(themePath, JSON.stringify(themeData, null, 2), 'utf-8');
+//     } catch (err) {
+//         console.log('error while setting side bar background: ' + (err as Error).message);
+//     }
+// }
 
-function changePanel(color: string, context: vscode.ExtensionContext) {
-    const themePath = path.join(context.extensionPath, 'themes', 'extensiontheme.json');
-    try {
-        const themeData = JSON.parse(fs.readFileSync(themePath, 'utf-8'));
-        themeData.colors['panel.background'] = color;
-        fs.writeFileSync(themePath, JSON.stringify(themeData, null, 2), 'utf-8');
-    } catch (err) {
-        console.log('error while setting panel background: ' + (err as Error).message);
-    }
-}
+// function changePanel(color: string, context: vscode.ExtensionContext) {
+//     const themePath = path.join(context.extensionPath, 'themes', 'extensiontheme.json');
+//     try {
+//         const themeData = JSON.parse(fs.readFileSync(themePath, 'utf-8'));
+//         themeData.colors['panel.background'] = color;
+//         fs.writeFileSync(themePath, JSON.stringify(themeData, null, 2), 'utf-8');
+//     } catch (err) {
+//         console.log('error while setting panel background: ' + (err as Error).message);
+//     }
+// }
 
-function changeStatusBar(color: string, context: vscode.ExtensionContext) {
-    const themePath = path.join(context.extensionPath, 'themes', 'extensiontheme.json');
-    try {
-        const themeData = JSON.parse(fs.readFileSync(themePath, 'utf-8'));
-        themeData.colors['statusBar.background'] = color;
-        fs.writeFileSync(themePath, JSON.stringify(themeData, null, 2), 'utf-8');
-    } catch (err) {
-        console.log('error while setting status bar background: ' + (err as Error).message);
-    }
-}
+// function changeStatusBar(color: string, context: vscode.ExtensionContext) {
+//     const themePath = path.join(context.extensionPath, 'themes', 'extensiontheme.json');
+//     try {
+//         const themeData = JSON.parse(fs.readFileSync(themePath, 'utf-8'));
+//         themeData.colors['statusBar.background'] = color;
+//         fs.writeFileSync(themePath, JSON.stringify(themeData, null, 2), 'utf-8');
+//     } catch (err) {
+//         console.log('error while setting status bar background: ' + (err as Error).message);
+//     }
+// }
 
-function changeTabs(color: string, context: vscode.ExtensionContext) {
-    const themePath = path.join(context.extensionPath, 'themes', 'extensiontheme.json');
-    try {
-        const themeData = JSON.parse(fs.readFileSync(themePath, 'utf-8'));
-        themeData.colors['editorGroupHeader.background'] = color;
-        fs.writeFileSync(themePath, JSON.stringify(themeData, null, 2), 'utf-8');
-    } catch (err) {
-        console.log('error while setting status bar background: ' + (err as Error).message);
-    }
-}
+// function changeTabs(color: string, context: vscode.ExtensionContext) {
+//     const themePath = path.join(context.extensionPath, 'themes', 'extensiontheme.json');
+//     try {
+//         const themeData = JSON.parse(fs.readFileSync(themePath, 'utf-8'));
+//         themeData.colors['editorGroupHeader.background'] = color;
+//         fs.writeFileSync(themePath, JSON.stringify(themeData, null, 2), 'utf-8');
+//     } catch (err) {
+//         console.log('error while setting status bar background: ' + (err as Error).message);
+//     }
+// }
 
-export { changeEditor, changeSidebar, changePanel, changeStatusBar, changeTabs, applyPreset};
+export { applyPreset};
