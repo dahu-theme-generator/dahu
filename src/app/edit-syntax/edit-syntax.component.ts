78174add9c4b41349ea27c33135d2575a6ed3722
andojasl <a.lapinskas@student.fontys.nl>
2024-07-14 22:34:40 +0300

    retrieve syntax scope names and hex values

--- src/app/edit-syntax/edit-syntax.component.ts
+++ src/app/edit-syntax/edit-syntax.component.ts
@@ -1,41 +1,58 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+
+import { VsCodeApiService } from 'src/vsCodeAPIService';
+import { Preset } from 'ext-src/dataObjects';
+
 @Component({
   selector: 'app-edit-syntax',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './edit-syntax.component.html',
-  styleUrl: './edit-syntax.component.css'
+  styleUrls: ['./edit-syntax.component.css']
 })
 export class EditSyntaxComponent {
-  colors = [
-    { name: 'Color 1', hex: '#FF0000' },
-    { name: 'Color 2', hex: '#00FF00' },
-    { name: 'Color 3', hex: '#0000FF' },
-    { name: 'Color 4', hex: '#FFFF00' },
-    { name: 'Color 5', hex: '#FF00FF' },
-    { name: 'Color 6', hex: '#008000' },
-    { name: 'Color 7', hex: '#800080' },
-    { name: 'Color 8', hex: '#808000' },
-    { name: 'Color 9', hex: '#800000' },
-    { name: 'Color 10', hex: '#008080' }
-  ];
+  private vscode: any;
 
-  displayedColors: { name: string; hex: string; }[] = [];
+  syntaxColors: { scope: string | string[], settings: { foreground: string } }[] = [];
+  displayedColors: { scope: string | string[], settings: { foreground: string } }[] = [];
   currentPage = 0;
+  currentTheme: Preset = {
+    id: 0,
+    name: '',
+    editorColor: '',
+    sidebarColor: '',
+    panelColor: '',
+    statusBarColor: '',
+    tabsColor: '',
+    tokenColors: ''
+  };
 
-  constructor() {
-    this.updateDisplayedColors();
+  constructor(vsCodeApiService: VsCodeApiService) {
+    this.vscode = vsCodeApiService.getVsCodeApi();
+    this.getCurrentTheme();
+    window.addEventListener('message', event => {
+      const message = event.data;
+      if (message.command === 'currentTheme') {
+        this.currentTheme = message.data;
+        this.syntaxColors = JSON.parse(this.currentTheme.tokenColors);
+        this.updateDisplayedColors();
+      }
+    });
+  }
+
+  getCurrentTheme() {
+    this.vscode.postMessage({ command: 'getCurrentTheme' });
   }
 
   updateDisplayedColors() {
     const startIndex = this.currentPage * 5;
-    this.displayedColors = this.colors.slice(startIndex, startIndex + 5);
+    this.displayedColors = this.syntaxColors.slice(startIndex, startIndex + 5);
   }
 
   nextPage() {
-    if ((this.currentPage + 1) * 5 < this.colors.length) {
+    if ((this.currentPage + 1) * 5 < this.syntaxColors.length) {
       this.currentPage++;
       this.updateDisplayedColors();
     }
@@ -49,10 +66,25 @@ export class EditSyntaxComponent {
   }
 
   updateColor(newHex: string, index: number) {
-    this.colors[index].hex = newHex;
+    const globalIndex = this.currentPage * 5 + index;
+    if (this.syntaxColors[globalIndex] && this.syntaxColors[globalIndex].settings) {
+      this.syntaxColors[globalIndex].settings.foreground = newHex;
+    }
   }
 
   saveColors() {
-    console.log('Colors saved:', this.colors);
+    // const updatedTokenColors = JSON.stringify(this.syntaxColors, null, 2);
+    // this.currentTheme.tokenColors = updatedTokenColors;
+   // this.vscode.postMessage({command: 'updatePreset', data: this.currentTheme});
+
+    this.vscode.postMessage({ command: 'updatePreset', data: this.currentTheme });
+  }
+
+  getScopeName(scope: string | string[]): string {
+    if (Array.isArray(scope)) {
+      return scope.map(s => s.replace(/,/g, ', ')).join('<br>'); // Join with line breaks
+    } else {
+      return scope.replace(/,/g, ', '); // Replace commas with commas followed by space
+    }
   }
 }
